5515b616de64c797a0f866187e13bc9779ec30d1
tonoxic <tonoxic@DESKTOP-IM22BE2.localdomain>
2020-04-03 05:18:19 -0400

    the locations of where the files are saved needs to be changed.  hopefully replaced with the drive api

--- src/app.py
+++ src/app.py
@@ -1,6 +1,16 @@
 from flask import Flask, request, jsonify, render_template, make_response
+from flask_pymongo import PyMongo
+from werkzeug.utils import secure_filename
+from datetime import datetime
+import os
+import json
 
 app = Flask(__name__)
+app.config["MONGO_URI"]='mongodb://localhost:27017/fruit_identifier'
+TEMPLATES_AUTO_RELOAD = True
+mongo=PyMongo(app)
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 @app.route("/test")
 def hello():
@@ -13,4 +23,34 @@ def home():
 @app.route("/upload")
 def uploadPage():
     return render_template("upload.html")
+
+@app.route("/upload", methods=['POST', 'GET'])
+def upload_image():
+    #need to change where the image is stored.  temporary
+    if request.method=="POST":
+        target = os.path.join(APP_ROOT, 'static/unverified-fruit')  
+        if not os.path.isdir(target):
+            os.mkdir(target) 
+        table = mongo.db.unverified_fruits
+        if 'upload_image' in request.files:
+            upload = request.files["upload_image"]
+            filename = secure_filename(upload.filename)
+            filename = str(datetime.timestamp(datetime.now()))+filename
+            destination = "/".join([target, filename])
+            upload.save(destination)
+            table.insert({'fruit_image': "/".join(["../static/unverified-fruit", filename])})
+            print("Insert sucessfull.")
+    return uploadPage()
+
+@app.route("/verify")
+def get_images():
+    target = os.path.join(APP_ROOT, 'unverified-fruit')  
+    table = mongo.db.unverified_fruits
+    pictures=[]
+    for doc in table.find({}):
+        pictures += ([doc])
+    print(pictures)
+    return render_template("verify.html", images=pictures)
+
+
 
